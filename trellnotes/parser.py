339b6aa3a64e9894401190450a83f298acf89927
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-04-04 07:18:25 -0400

    Started adding Classes for each items

--- trellnotes/parser.py
+++ trellnotes/parser.py
@@ -7,7 +7,8 @@ class BoardData:
     def __init__(self, src):
         self.src = src
         self.data = self.load_json()
-        pass
+        self.members = self.instantiate_object('members', Member)
+        self.lists = self.instantiate_object('lists', CardList)
 
     def load_json(self):
         """loads the json file into a data structre"""
@@ -15,3 +16,53 @@ class BoardData:
             file_data = data_file.read()
         board_data = json.loads(file_data)
         return board_data
+
+    def instantiate_object(self, data_id, class_name):
+        """Parses through and instantiates all objects for a class."""
+        object_list = {}
+        for item_data in self.data[data_id]:
+            obj = class_name(item_data)
+            object_list[obj.trelloid] = obj
+        return object_list
+
+
+class Member:
+    """A Trello Board Member"""
+    def __init__(self, data):
+        self.trelloid = data['id']
+        self.username = data['username']
+        self.full_name = data['fullName']
+        self.url = data['url']
+        self.bio = data['bio']
+
+
+class CardList:
+    """A List of Trello Cards"""
+    def __init__(self, data):
+        self.trelloid = data['id']
+        self.name = data['name']
+        self.closed = data['closed']
+        self.position = data['pos']
+
+class Label:
+    """A Trello Label"""
+    def __init__(self, data):
+        pass
+
+
+class Card:
+    """A Trello Card"""
+    def __init__(self, data):
+        pass
+
+
+class checklist:
+    """A Trello Checklist"""
+    def __init__(self, data):
+        pass
+
+
+class Action:
+    """A Trello Action"""
+    def __init__(self, data):
+        pass
