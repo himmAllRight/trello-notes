d5ee3d9d725a06a7285af14d4479e68eac79293a
Ryan Himmelwright <ryan.himmelwright@gmai.com>
2019-04-04 21:00:47 -0400

    Started tests but need to fix importing

--- trellnotes/tests/test_board_data.py
+++ trellnotes/tests/test_board_data.py
@@ -1,7 +1,25 @@
-# import parser
+import trellnotes.parser
+import pytest
+
+# Global variable of test input file
+test_imput_src = 'test-data/test-board-export.json'
 
 
 # Just until I get a better sample input file I can use to test what I already
-# have.
-def test_data():
-    assert 1 == 1
+# have.pytest.fixture
+def test_board():
+    loaded_board = trellnotes.parser.BoardData(test_imput_src)
+    return loaded_board
+
+
+def test_data(test_board):
+    assert test_board.src == test_imput_src
+
+
+def test_members(test_board):
+    """Tests the member list and objects are loaded correctly"""
+    assert len(test_board.members) == 1
+
+    member = test_board.members[0]
+    assert member.username == 'ryanhimmelwright'
+    assert member.full_name == 'Ryan Himmelwright'
