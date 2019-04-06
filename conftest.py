2bf89dabc4dbd5ebdfe05188e8250a13bb321119
Ryan Himmelwright <ryan.himmelwright@gmai.com>
2019-04-06 08:30:09 -0400

    Cleaned up some of the testing stuff

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 
 test_imput_src = 'trellnotes/tests/test-data/test-board-export.json'
-print("hello")
+
 
 @pytest.fixture
 def test_board():
--- trellnotes/tests/test_board_data.py
+++ trellnotes/tests/test_board_data.py
@@ -5,17 +5,6 @@ import pytest
 test_imput_src = '/home/ryan/Documents/Programming/python/trello-notes/trellnotes/tests/test-data/test-board-export.json'
 
 
-# Just until I get a better sample input file I can use to test what I already
-# have.pytest.fixture
-def test_board():
-    loaded_board = BoardData(test_imput_src)
-    return loaded_board
-
-
-# def test_data(test_board):
-    # assert test_board.src == test_imput_src
-
-
 def test_members(test_board):
     """Tests the member list and objects are loaded correctly"""
     assert len(test_board.members) == 1
